7870b8bb42f6b6512acdb2cbbe9c1036626dcdb1
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-10-08 16:27:56 +0530

    icon added design

--- src/HomePage.js
+++ src/HomePage.js
@@ -10,28 +10,36 @@ import Slider from './slider/Slider'
 const HomePage = ({ navigation}) => {
     return (
         <View style={styles.container}>
-            <ImageBackground imageStyle={{borderBottomRightRadius: 70, }} style={styles.imageHotel} source={require('./travel.jpg')}>
+            <ImageBackground imageStyle={{borderBottomRightRadius: 70, }} style={styles.imageHotel}  source={{uri:'https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80'}}>
             <View style={styles.imageHotelContent}>
             <View style={styles.subContainer}>
             <View style={styles.title}>
-                <Text style={{fontSize:22,fontWeight: 'bold',color:'white'}}>Hi</Text>
-                <Text style={{ fontSize: 22, fontFamily: liteFont,color:'orange',marginLeft:6}}>Faheem</Text>
+                <Text style={{fontSize:24,fontFamily:liteFont,color:'white'}}>Hi</Text>
+                <Text style={{ fontSize: 22, fontFamily: baseFont,color:'orange',marginLeft:6}}>Faheem,</Text>
                 <View style={{paddingLeft:190,alignItems: 'center'}}>
                 <Icon name={"bell-ring"} size={26} color={'orange'} />
                 </View>
 
             </View>
             <View style={{flexDirection: 'row',alignItems: 'center',}}>
-            <Text style={{ fontSize: 22, fontFamily: liteFont,color:'white'}}>Where do u want to</Text>
+            <Text style={{ fontSize: 24, fontFamily: liteFont,color:'white'}}>Where do u want to</Text>
             <Text  style={{ fontSize: 24, fontFamily: liteFont,color:'orange',marginLeft:6}}>go </Text>
             <Text style={{  fontSize: 25, fontFamily: liteFont,color:'#fff'}}>?</Text>
             </View>
-            <View style={{ elevation:1}}>
             <TouchableOpacity style={styles.searchView} activeOpacity={0.7}>
             <Icon name={"magnify"} size={22} color={'orange'} />
                 <Text style={{ color: '#f1f5f9',fontFamily: baseFont,fontSize:16,paddingLeft:5}}>Search Places..</Text>
+            
             </TouchableOpacity>
-        </View>
+            <View style={styles.description}>
+            <Icon name={"walk"} size={12} color={'white'} />
+            <Icon name={"bike"} size={15} color={'orange'} />
+            <Icon name={"car-hatchback"} size={22} color={'white'} />
+            <Icon name={"bus-side"} size={24} color={'orange'} />
+            <Icon name={"train"} size={28} color={'white'} />
+            <Icon name={"sail-boat"} size={32} color={'orange'} />
+            <Icon name={"airplane-takeoff"} size={34} color={'white'} />
+            </View>
         </View>
         </View>
         </ImageBackground>
@@ -64,7 +72,7 @@ const styles = StyleSheet.create({
     },
     imageHotelContent:
     {
-        backgroundColor: 'rgba(52, 52, 52, 0.4)',
+        backgroundColor: 'rgba(52, 52, 52, 0.5)',
         height: 250,
         borderBottomRightRadius: 70,
     },
@@ -76,6 +84,11 @@ const styles = StyleSheet.create({
         marginVertical:25,
         flexDirection: 'row',
         alignItems: 'center',
+    },
+    description: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     }
 
 })
