89bff81ade39f293d87ac0bea46038fe84c95b52
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-10-09 22:45:31 +0530

    ngrok URL update

--- src/global.js
+++ src/global.js
@@ -3,6 +3,6 @@ baseFont = 'Mulish-Regular';
 liteFont = 'Mulish-Bold';
 extraFont = 'Mulish-ExtraBold'
 
-const baseUrl = "https://592f-43-229-90-56.ngrok.io/"
+const baseUrl = "https://9ea6-43-229-88-239.ngrok.io/"
 
 export default {baseUrl}
