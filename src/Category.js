4f62899d94244f6d3c557eb96a34161b0443739f
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-10-12 12:51:20 +0530

    search filter added

--- src/Category.js
+++ src/Category.js
@@ -1,11 +1,54 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+const Category = () => {
+
+    const [search, setSearch] = useState('')
+
+    const filterList = (list) => {
+        return list.filter(listItem => listItem.toLowerCase().includes(search.toLowerCase()));
+
+    }
+    const list = ['The Weeknd', 'Drake', 'Roddy Ricch', 'Dua Lipa','faheem'];
 
 
-export const Category = () => {
     return (
-        <Text>didh</Text>
+        <View style={styles.container}>
+            <TextInput
+                onChangeText={(search) => setSearch(search)}
+                style={styles.searchBar}
+            />
+            {filterList(list).map((list, index) => (
+                    <Text key={index} style={styles.itemText}>{list}</Text>
+                ))}
+        </View>
     )
+
 }
 
+
 export default Category
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+        alignItems: 'center',
+        height: '100%',
+    },
+    searchBar: {
+        fontSize: 24,
+        margin: 10,
+        width: '90%',
+        height: 50,
+        backgroundColor: 'white',
+    },
+    itemText: {
+        margin: 10,
+        color: 'white',
+        fontSize: 24,
+        backgroundColor: 'blue',
+        width: '100%',
+        height: 50
+    }
+});
