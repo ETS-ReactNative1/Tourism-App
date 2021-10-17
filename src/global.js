48990525d599abc78466ea6b8ec6aae74add2c5e
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-10-17 12:43:45 +0530

    URL changed

--- src/global.js
+++ src/global.js
@@ -3,6 +3,6 @@ baseFont = 'Mulish-Regular';
 liteFont = 'Mulish-Bold';
 extraFont = 'Mulish-ExtraBold'
 
-const baseUrl = "https://144e-43-229-90-85.ngrok.io/"
+const baseUrl = "https://3530-103-70-197-222.ngrok.io/"
 
 export default {baseUrl}
