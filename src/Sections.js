b777d674518ce79c1e3b6524efe2be98fc682291
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-10-27 20:47:44 +0530

    user post to get page function added

--- src/Sections.js
+++ src/Sections.js
@@ -1,73 +1,98 @@
-import React from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ReadMore from '@fawazahmed/react-native-read-more';
 
+import './global';
+import global from './global'
+import axios from 'axios'
 
 const Sections = () => {
 
 
+    const [user, setUser] = useState([])
+    const [loading, setLoading] = useState(true)
 
-    const list = [{
-        id: 1, name: 'Muhammed faheem', location: 'Pathiriyal', description: 'giwefeuwgfuiehfoeofhweoifheoihweoigiwefeuwgfuiehfoeofhweoifheoihwgiwefeuwgfuiehfoeofhweoifheoihwgiwefeuwgfuiehfoeofhweoifheoihw'
-        , rating: 7, image: 'https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80'
-    },
-    {
-        id: 2, name: 'Muhammed anshab', location: 'Wayanad', description: 'giwefeuwwl;f;wejfpoewjfoefwefewjlgfuiehfoeofhweoifheoihweoi'
-        , rating: 8, image: 'https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80'
-    },
-    {
-        id: 3, name: 'Muhammed anshab', location: 'Wayanad', description: 'giwefeuwwl;f;wejfpoewjfoefwefewjlgfuiehfoeofhweoifheoihweoi'
-        , rating: 8, image: 'https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80'
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    const getData = () => {
+        var config = {
+            method: 'get',
+            url: global.baseUrl + 'api/users/',
+            // headers: {}
+        };
+
+        axios(config)
+            .then(function (response) {
+                if (response.data.status === 200) {
+                    setUser(response.data.data)
+                    setLoading(false)
+                } else {
+                    console.warn('No Data')
+                }
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
     }
-    ]
 
 
+
+
+
+    if (loading) {
+        return <View style={{ ...styles.container, justifyContent: 'center', alignItems: 'center' }}><ActivityIndicator size="large" color="orange" /></View>
+    }
     return (
         <View style={styles.container}>
             <View style={styles.navBar}>
                 <Text style={{ color: 'orange', fontFamily: boldFont, fontSize: 20 }}>Users Post</Text>
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
-                {list.map(items => 
-                <View style={styles.card} key={items.id}>
-                    <View style={styles.cardTitle}>
-                        <View style={{ padding: '2%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                        <View style={styles.image2Container}>
-                            <Image style={styles.image2} source={{ uri:'https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80'}}>
+                {user.map(items =>
+                    <View style={styles.card} key={items.id}>
+                        <View style={styles.cardTitle}>
+                            <View style={{ padding: '2%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                                <View style={styles.image2Container}>
+                                    <Image style={styles.image2} source={{uri:items.profile}}>
 
-                            </Image>
-                            </View>
-                            <View style={{ marginRight: 125 }}>
-                                <Text style={{ color: 'black', fontFamily: boldFont, fontSize: 16 }}>{items.name}</Text>
-                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Icon name={"map-marker-radius"} size={14} color={'black'} />
-                                    <Text style={{ color: 'black', fontFamily: liteFont, fontSize: 12 }}>{items.location}</Text>
+                                    </Image>
                                 </View>
+                                <View style={{ marginRight: 125 }}>
+                                    <Text style={{ color: 'black', fontFamily: boldFont, fontSize: 16 }}>{items.name}</Text>
+                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                        <Icon name={"map-marker-radius"} size={14} color={'black'} />
+                                        <Text style={{ color: 'black', fontFamily: liteFont, fontSize: 12 }}>{items.place}</Text>
+                                    </View>
+                                </View>
+                                <TouchableOpacity>
+                                    <Icon name={"dots-vertical"} size={24} color={'black'} />
+                                </TouchableOpacity>
                             </View>
-                            <TouchableOpacity>
-                                <Icon name={"dots-vertical"} size={24} color={'black'} />
-                            </TouchableOpacity>
                         </View>
-                    </View>
-                    <View style={styles.imageContainer}>
-                        <Image style={styles.image} source={{ uri: 'https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80' }} />
-                    </View>
-                    <View style={styles.description}>
-                        <ReadMore numberOfLines={3} style={{ fontFamily: boldFont }} >
-                            {
-                                <Text>{items.description}</Text>
+                        <View style={styles.imageContainer}>
+                            <Image style={styles.image} source={{uri:items.image}} />
+                        </View>
+                        <View style={styles.description}>
+                            <ReadMore numberOfLines={3} style={{ fontFamily: boldFont }} >
+                                {
+                                    <Text>{items.experience}</Text>
 
-                            }
-                        </ReadMore>
-                    </View>
-                    <View style={styles.rating}>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Icon name={"star-half-full"} size={22} color={'green'} />
-                            <Text style={{ fontSize: 18, color: 'green' }}>{items.rating}/9</Text>
+                                }
+                            </ReadMore>
                         </View>
-                    </View>
-                </View>)}
+                        <View style={styles.rating}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <Icon name={"star-half-full"} size={22} color={'green'} />
+                                <Text style={{ fontSize: 18, color: 'green' }}>{items.rating}/9</Text>
+                            </View>
+                        </View>
+                    </View>)}
             </ScrollView>
 
         </View>
