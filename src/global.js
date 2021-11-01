4754df58957ca54fbd41bd47d27e868516dd64e8
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-11-01 14:36:14 +0530

    reviews url chnged

--- src/global.js
+++ src/global.js
@@ -3,6 +3,6 @@ baseFont = 'Mulish-Regular';
 liteFont = 'Mulish-Bold';
 extraFont = 'Mulish-ExtraBold'
 
-const baseUrl = "https://f348-103-70-197-144.ngrok.io/"
+const baseUrl = "https://faheemk.pythonanywhere.com/"
 
 export default {baseUrl}
