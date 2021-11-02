1d2f63bcca97cc51ada47dc3e5ee0d2dae4e4224
Muhammed Faheem <56709898+faheem-cmd@users.noreply.github.com>
2021-11-02 19:41:17 +0530

    Internet permission added

--- src/global.js
+++ src/global.js
@@ -3,6 +3,6 @@ baseFont = 'Mulish-Regular';
 liteFont = 'Mulish-Bold';
 extraFont = 'Mulish-ExtraBold'
 
-const baseUrl = "http://faheemk.pythonanywhere.com/"
+const baseUrl = "https://faheemk.pythonanywhere.com/"
 
 export default {baseUrl}
